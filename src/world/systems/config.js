c9855cfe7fac4f09c4e2532bd64525dfec64d4cf
Alex <104495043+GhostfromUkraine@users.noreply.github.com>
2024-07-09 20:20:52 +0200

    test

--- src/world/systems/config.js
+++
@@ -1,6 +0,0 @@
-const aspect = 1.2;
-const cameraZ = 300;
-const canvasWidth = () => window.innerWidth / 1.8;
-const canvasHeight = () => canvasWidth() / aspect;
-
-export { aspect, cameraZ, canvasWidth, canvasHeight };
